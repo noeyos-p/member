5f10e160d1c6a92f507971f10702297526dbbb43
noeyos-p <krapnoeyos@gmail.com>
2025-08-14 15:22:58 +0900

    1. insertForm.html 생성 2. service 구현 및 저장 완료

---
+++ src/main/java/com/my/member/service/MemberService.java
@@ -0,0 +1,57 @@
+package com.my.member.service;
+
+import com.my.member.dto.MemberDto;
+import com.my.member.entity.Member;
+import com.my.member.repository.MemberRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class MemberService {
+    // repository 를 생성자 주입 방법으로 가져오기
+    private final MemberRepository repository;
+    // 태어날때부터 MemberRepository를 갖고 태어남
+
+    public MemberService(MemberRepository repository) {
+        this.repository = repository;
+    }
+
+
+    // repository 통해서 멤버 리스트 가져오기
+    public List<MemberDto> getAllList() {
+        List<Member> memberList = repository.findAll();
+        System.out.println(memberList);
+        // 비어있는 Dto List 만들기
+        /*List<MemberDto> dtoList = new ArrayList<>();*/
+        // Entity List를 Dto List로 변환
+        /*for (int i = 0; i < memberList.size(); i++) {
+            // 리스트에 있는 엔티티를 하나씩 읽어서
+            // Dto에 담는다.
+            MemberDto dto = new MemberDto();
+            dto.setId(memberList.get(i).getId());
+            dto.setName(memberList.get(i).getName());
+            dto.setAge(memberList.get(i).getAge());
+            dto.setAddress(memberList.get(i).getAddress());
+            dtoList.add(dto);
+        }*/
+        // fromMemberEntity 메서드로 작업하기
+        return memberList
+                .stream()
+                .map(x -> MemberDto.fromMemberEntity(x))
+                .toList();
+        /*return dtoList;*/
+    }
+
+    public void insertMember(MemberDto dto) {
+        // 3. 서비스에서 DTO를 엔티티로 바꾼다.
+        Member member = new Member();
+        member.setName(dto.getName());
+        member.setAge(dto.getAge());
+        member.setAddress(dto.getAddress());
+        // 4. 리포지토리를 이용해서 저장한다.
+        member = repository.save(member);
+        System.out.println("-------------------------------------");
+        System.out.println(member);
+    }
+}
