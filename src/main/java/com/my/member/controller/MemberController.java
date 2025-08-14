21ba1190f5b56a240486c6c89d0eab2a03a16483
noeyos-p <krapnoeyos@gmail.com>
2025-08-14 17:08:15 +0900

    1. 수정 작업 완료 2. th:field를 사용한 input이 disable 이면 hidden으로 보내야 함

--- src/main/java/com/my/member/controller/MemberController.java
+++ src/main/java/com/my/member/controller/MemberController.java
@@ -7,10 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.ObjectUtils;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -61,9 +58,16 @@ public class MemberController {
         if (ObjectUtils.isEmpty(dto)) {
             return "redirect:/list";
         } else {
+            // 3. 받은 DTO를 수정 폼으로 보낸다.
             model.addAttribute("dto", dto);
             return "updateForm";
         }
-        // 3. 받은 DTO를 수정 폼으로 보낸다.
+    }
+
+    @PostMapping("/member/update")
+    public String update(@ModelAttribute("dto")MemberDto dto) {
+        System.out.println(dto);
+        service.updateMember(dto);
+        return "redirect:/list";
     }
 }
